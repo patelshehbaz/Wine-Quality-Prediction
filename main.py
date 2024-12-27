dd1545de518f5b90da479d6258a8fd40073e9ff2
Shehbaz Patel <60441352+patelshehbaz@users.noreply.github.com>
2024-12-27 00:46:40 -0500

    Data ingestion pipeline created

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from Wine_Quality_Project  import logger
+from Wine_Quality_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to ml-project")
-logger.info("Welcome to channel")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
